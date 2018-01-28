80092af35a56ff586a5e610662bd8aa91c3958b4
Sandro Lutz <sandro.lutz@temparus.ch>
2018-01-28 14:23:21 +0100

    Add command line argument parser

--- gitlab-github-sync.py
+++ gitlab-github-sync.py
@@ -10,4 +10,16 @@ import ipaddress
 import json
 from utils import *
 
-# Code should come here...
+parser = argparse.ArgumentParser(prog='gitlab-github-sync',
+          description='Synchronizes repositories between GitLab and GitHub w/o direct access to the GitLab Server.')
+parser.add_argument('-v', '--verbose', action="store_true",
+                   help='prints more output to the console')
+parser.add_argument('-c', '--config', metavar='config.json', nargs=1, type=argparse.FileType('r'),
+                   default='config.json', help='path to the configuration file')
+parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+args = parser.parse_args()
+
+# Read configuration file
+data = json.load(args.config)
+
+# Perform sync here...
